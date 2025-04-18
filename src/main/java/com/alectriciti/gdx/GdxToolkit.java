0e8acce538ff1bb626100f59c2718ca7d6265744
Alectriciti <Alectriciti@gmail.com>
2025-04-18 16:10:14 -0400

    Initial commit

---
+++ src/main/java/com/alectriciti/gdx/GdxToolkit.java
@@ -0,0 +1,13 @@
+package com.alectriciti.gdx;
+
+import com.badlogic.gdx.Gdx;
+
+public class GdxToolkit {
+	
+	public static void Test() {
+		System.out.println("poop");
+		System.out.println("screen width is totally like "+Gdx.graphics.getWidth());
+		
+	}
+
+}
